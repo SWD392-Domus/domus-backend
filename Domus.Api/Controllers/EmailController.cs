8475ce039deef06cfade8ac62d66d8efc6bf4aa8
HaiHungNguyenn <hainhse173100@fpt.edu.vn>
2024-01-19 12:36:57 +0700

    [MailService][Hai] create sendmail service

---
+++ Domus.Api/Controllers/EmailController.cs
@@ -0,0 +1,23 @@
+﻿using Domus.Api.Controllers.Base;
+using Domus.Service.Interfaces;
+using Domus.Service.Models.Email;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Domus.Api.Controllers;
+[Route("/api/[controller]")]
+public class EmailController : BaseApiController
+{
+    private readonly IEmailService _emailService;
+    public EmailController(IEmailService emailService)
+    {
+        _emailService = emailService;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> SendMail(Email email)
+    {
+        return await ExecuteServiceLogic(
+           async() => await _emailService.SendEmail(email).ConfigureAwait(false)
+        ).ConfigureAwait(false);
+    }
+}
---
+++ Domus.Service/Implementations/EmailService.cs
@@ -0,0 +1,60 @@
+﻿using System.Text.Json.Serialization;
+using Domus.Service.Interfaces;
+using Domus.Service.Models;
+using Domus.Service.Models.Email;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.Extensions.Configuration;
+using MimeKit;
+using MimeKit.Text;
+using Newtonsoft.Json;
+
+namespace Domus.Service.Implementations;
+
+public class EmailService : IEmailService
+{
+    private readonly IConfiguration _config ;
+
+    public EmailService(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    
+    
+    public async Task<ServiceActionResult> SendEmail(Email request)
+    {
+        var emailConfig = _config.GetSection(nameof(EmailConfig)).Get<EmailConfig>() ?? throw new Exception();
+        var email = new MimeMessage();
+        email.From.Add(MailboxAddress.Parse(emailConfig.EmailUsername));
+        email.To.Add(MailboxAddress.Parse(request.To));
+        email.Subject = request.Subject;
+        email.Body = new TextPart(TextFormat.Html) { Text = request.EmailBody };
+        using var smtp = new SmtpClient();
+        await smtp.ConnectAsync(emailConfig.EmailHost, 587, SecureSocketOptions.StartTls);
+        await smtp.AuthenticateAsync(emailConfig.EmailUsername, emailConfig.EmailPassword);
+        await smtp.SendAsync(email);
+        await smtp.DisconnectAsync(true);
+        return new ServiceActionResult(true);
+    }
+
+    public async Task<ServiceActionResult> SendSeveralEmail(ServeralEmail serveralEmail)
+    {
+        foreach (var x in serveralEmail.To)
+        {
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse(_config.GetSection("EmailUsername").Value));
+            email.To.Add(MailboxAddress.Parse(x.ToString()));
+            email.Subject = serveralEmail.Subject;
+            email.Body = new TextPart(TextFormat.Html) { Text = serveralEmail.EmailBody };
+
+            using var smtp = new SmtpClient();
+            await smtp.ConnectAsync(_config.GetSection("EmailHost").Value, 587, SecureSocketOptions.StartTls);
+            await smtp.AuthenticateAsync(_config.GetSection("EmailUsername").Value, _config.GetSection("EmailPassword").Value);
+            await smtp.SendAsync(email);
+            await smtp.DisconnectAsync(true);
+        }
+
+        return new ServiceActionResult(true);
+    }
+}
