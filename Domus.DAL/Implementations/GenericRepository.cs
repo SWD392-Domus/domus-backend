e7ac7644b665666fb432ee0856cf81c58545e304
duykasama <duykasama@gmail.com>
2024-01-22 16:56:50 +0700

    [Product details management][Duy] Update generic repository

--- Domus.DAL/Implementations/GenericRepository.cs
+++ Domus.DAL/Implementations/GenericRepository.cs
@@ -7,30 +7,32 @@ namespace Domus.DAL.Implementations;
 public abstract class GenericRepository<T> : IGenericRepository<T> where T : class 
 {
 	private readonly IAppDbContext _dbContext;
+	private readonly DbSet<T> _dbSet;
 
     protected GenericRepository(IAppDbContext dbContext)
     {
         _dbContext = dbContext;
+		_dbSet = _dbContext.CreateSet<T>();
     }
 
     public void Add(T entity)
     {
-        _dbContext.CreateSet<T>().AddAsync(entity);
+        _dbSet.AddAsync(entity);
     }
 
     public async Task AddAsync(T entity)
     {
-        await _dbContext.CreateSet<T>().AddAsync(entity);
+        await _dbSet.AddAsync(entity);
     }
 
     public void AddMany(IEnumerable<T> entities)
     {
-        _dbContext.CreateSet<T>().AddRange(entities);
+        _dbSet.AddRange(entities);
     }
 
     public async Task AddManyAsync(IEnumerable<T> entities)
     {
-        await _dbContext.CreateSet<T>().AddRangeAsync(entities);
+        await _dbSet.AddRangeAsync(entities);
     }
 
     public void Update(T entity)
@@ -54,7 +56,7 @@ public abstract class GenericRepository<T> : IGenericRepository<T> where T : cla
 
     public void UpdateMany(Expression<Func<T, bool>> predicate)
     {
-        var entities = _dbContext.CreateSet<T>().Where(predicate);
+        var entities = _dbSet.Where(predicate);
         foreach (var entity in entities)
         {
             _dbContext.Update<T>(entity);
@@ -73,69 +75,69 @@ public abstract class GenericRepository<T> : IGenericRepository<T> where T : cla
 
     public async Task UpdateManyAsync(Expression<Func<T, bool>> predicate)
     {
-        var entities = _dbContext.CreateSet<T>().Where(predicate);
+        var entities = _dbSet.Where(predicate);
         await entities.ForEachAsync(c => _dbContext.Update<T>(c));
     }
 
     public void DeleteMany(Expression<Func<T, bool>> predicate)
     {
-        var entities = _dbContext.CreateSet<T>().Where(predicate);
+        var entities = _dbSet.Where(predicate);
         entities.ForEachAsync(c => _dbContext.SetDeleted<T>(c));
     }
 
     public async Task DeleteManyAsync(Expression<Func<T, bool>> predicate)
     {
-        var entities = _dbContext.CreateSet<T>().Where(predicate);
+        var entities = _dbSet.Where(predicate);
         await entities.ForEachAsync(c => _dbContext.SetDeleted<T>(c));
     }
 
     public IQueryable<T> GetAll()
     {
-        return _dbContext.CreateSet<T>().AsQueryable();
+        return _dbSet.AsQueryable();
     }
 
     public async Task<IQueryable<T>> GetAllAsync()
     {
-        return await Task.FromResult(_dbContext.CreateSet<T>().AsQueryable());
+        return await Task.FromResult(_dbSet.AsQueryable());
     }
 
     public T? Get(Expression<Func<T, bool>> predicate)
     {
-        return _dbContext.CreateSet<T>().SingleOrDefault(predicate);
+        return _dbSet.SingleOrDefault(predicate);
     }
 
     public Task<T?> GetAsync(Expression<Func<T, bool>> predicate)
     {
-        return _dbContext.CreateSet<T>().FirstOrDefaultAsync(predicate);
+        return _dbSet.FirstOrDefaultAsync(predicate);
     }
 
     public IQueryable<T> Find(Expression<Func<T, bool>> predicate)
     {
-        return _dbContext.CreateSet<T>().Where(predicate).AsQueryable();
+        return _dbSet.Where(predicate).AsQueryable();
     }
 
     public async Task<IQueryable<T>> FindAsync(Expression<Func<T, bool>> predicate)
     {
-        return await Task.FromResult(_dbContext.CreateSet<T>().Where(predicate).AsQueryable());
+        return await Task.FromResult(_dbSet.Where(predicate).AsQueryable());
     }
 
     public long Count(Expression<Func<T, bool>> predicate)
     {
-        return _dbContext.CreateSet<T>().Where(predicate).Count();
+        return _dbSet.Where(predicate).Count();
     }
 
     public async Task<long> CountAsync(Expression<Func<T, bool>> predicate)
     {
-        return await _dbContext.CreateSet<T>().Where(predicate).CountAsync();
+        return await _dbSet.Where(predicate).CountAsync();
     }
 
     public bool Exists(Expression<Func<T, bool>> predicate)
     {
-        return _dbContext.CreateSet<T>().Where(predicate).Any();
+        return _dbSet.Where(predicate).Any();
     }
 
     public async Task<bool> ExistsAsync(Expression<Func<T, bool>> predicate)
     {
-        return await _dbContext.CreateSet<T>().Where(predicate).AnyAsync();
+        return await _dbSet.Where(predicate).AnyAsync();
     }
 }
