4551743f2b8ae0e9f6ce4e91becf3e4daaf1d75b
duykasama <duykasama@gmail.com>
2024-01-25 21:02:08 +0700

    [Quotations management][Duy] Create quotation

--- Domus.Service/Implementations/QuotationService.cs
+++ Domus.Service/Implementations/QuotationService.cs
@@ -3,6 +3,7 @@ using AutoMapper.QueryableExtensions;
 using Domus.Common.Helpers;
 using Domus.DAL.Interfaces;
 using Domus.Domain.Dtos;
+using Domus.Domain.Entities;
 using Domus.Service.Exceptions;
 using Domus.Service.Interfaces;
 using Domus.Service.Models;
@@ -16,17 +17,62 @@ public class QuotationService : IQuotationService
 	private readonly IQuotationRepository _quotationRepository;
 	private readonly IUnitOfWork _unitOfWork;
 	private readonly IMapper _mapper;
+	private readonly IUserRepository _userRepository;
+	private readonly IProductDetailRepository _productDetailRepository;
 
-	public QuotationService(IQuotationRepository quotationRepository, IUnitOfWork unitOfWork, IMapper mapper)
+	public QuotationService(
+			IQuotationRepository quotationRepository,
+			IUnitOfWork unitOfWork,
+			IMapper mapper,
+			IUserRepository userRepository,
+			IProductDetailRepository productDetailRepository)
 	{
 		_quotationRepository = quotationRepository;
 		_unitOfWork = unitOfWork;
+		_userRepository = userRepository;
+		_productDetailRepository = productDetailRepository;
 		_mapper = mapper;
 	}
 
-    public Task<ServiceActionResult> CreateQuotation(CreateQuotationRequest request)
+    public async Task<ServiceActionResult> CreateQuotation(CreateQuotationRequest request)
     {
-        throw new NotImplementedException();
+		if (!await _userRepository.ExistsAsync(u => u.Id == request.CustomerId))
+			throw new UserNotFoundException("Customer not found");
+		if (!await _userRepository.ExistsAsync(u => u.Id == request.StaffId))
+			throw new UserNotFoundException("Staff not found");
+
+		var quotation = _mapper.Map<Quotation>(request);
+		quotation.CustomerId = request.CustomerId;
+		quotation.StaffId = request.StaffId;
+		quotation.CreatedAt = DateTime.Now;
+		quotation.LastUpdatedAt = DateTime.Now;
+		quotation.ExpireAt = request.ExpireAt;
+
+		foreach (var productDetailId in request.ProductDetails)
+		{
+			var productDetail = await _productDetailRepository.GetAsync(q => q.Id == productDetailId);
+			if (productDetail == null)
+				throw new QuotationNotFoundException($"Product detail with id {productDetailId} not found");
+			else
+			{
+				var productDetailQuotation = new ProductDetailQuotation
+				{
+					QuotationId = quotation.Id,
+					ProductDetailId = productDetailId,
+					Price = productDetail.DisplayPrice,
+					MonetaryUnit = "VND",
+					Quantity = 1,
+					QuantityType = "unit"
+				};
+
+				quotation.ProductDetailQuotations.Add(productDetailQuotation);
+			}
+		}
+
+		await _quotationRepository.AddAsync(quotation);
+		await _unitOfWork.CommitAsync();
+
+		return new ServiceActionResult(true);
     }
 
     public async Task<ServiceActionResult> DeleteQuotation(Guid id)
