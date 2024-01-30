c45d2a063d57a130390d795477ccfc091dbd4618
duykasama <duykasama@gmail.com>
2024-01-30 11:27:34 +0700

    [GG Authen][Duy] Persist user's information to database

---
+++ Domus.Common/Constants/GoogleTokenClaimConstants.cs
@@ -0,0 +1,20 @@
+namespace Domus.Common.Constants;
+
+public static class GoogleTokenClaimConstants
+{
+	public const string ISS = "iss";
+	public const string AZP = "azp";
+	public const string AUD = "aud";
+	public const string SUB = "sub";
+	public const string HD = "hd";
+	public const string EMAIL = "email";
+	public const string EMAIL_VERIFIED = "email_verified";
+	public const string AT_HASH = "at_hash";
+	public const string NAME = "name";
+	public const string PICTURE = "picture";
+	public const string GIVEN_NAME = "given_name";
+	public const string FAMILY_NAME = "family_name";
+	public const string LOCALE = "locale";
+	public const string IAT = "iat";
+	public const string EXP = "exp";
+}
