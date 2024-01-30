e0b65753033db5cbdb418de0518d53664f92417a
HaiHungNguyenn <hainhse173100@fpt.edu.vn>
2024-01-30 09:34:16 +0700

    [PackageManagement][Hai] add package image and mapping  entity

---
+++ Domus.Domain/Dtos/DtoPackageImage.cs
@@ -0,0 +1,12 @@
+﻿namespace Domus.Domain.Dtos;
+
+public class DtoPackageImage
+{
+    public Guid Id { get; set; }
+
+    public string ImageUrl { get; set; } = null!;
+
+    public int? Width { get; set; }
+
+    public int? Height { get; set; }
+}
---
+++ Domus.Domain/Entities/PackageImage.cs
@@ -0,0 +1,16 @@
+﻿using Domus.Domain.Entities.Base;
+
+namespace Domus.Domain.Entities;
+
+public partial class PackageImage : BaseEntity<Guid>
+{
+    public Guid PackageId { get; set; }
+    
+    public string ImageUrl { get; set; } = null!;
+
+    public int? Width { get; set; }
+
+    public int? Height { get; set; }
+    
+    public virtual Package Package { get; set; } = null!;
+}
