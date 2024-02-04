c737696effaaf4811b505ff74d20c10f21d4904b
duykasama <duykasama@gmail.com>
2024-02-04 23:36:06 +0700

    [Refactor][Duy] Add authentication to controllers and refactor code

--- Domus.Api/Controllers/ProductsController.cs
+++ Domus.Api/Controllers/ProductsController.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Domus.Api.Controllers;
 
-[Authorize(Roles = $"{UserRoleConstants.ADMIN},{UserRoleConstants.STAFF}", AuthenticationSchemes = "Bearer")]
+[Authorize(Roles = UserRoleConstants.INTERNAL_USER, AuthenticationSchemes = "Bearer")]
 [Route("api/[controller]")]
 public class ProductsController : BaseApiController
 {
--- Domus.Service/Constants/UserRoleConstants.cs
+++ Domus.Service/Constants/UserRoleConstants.cs
@@ -5,5 +5,6 @@ public static class UserRoleConstants
 	public const string ADMIN = "Admin";
     // public const string MANAGER = "Manager";
 	public const string STAFF = "Staff";
+	public const string INTERNAL_USER = ADMIN + "," + STAFF;
     public const string CLIENT = "Client";
 }
