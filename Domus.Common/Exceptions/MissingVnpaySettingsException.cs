3040f8bd2870ae0264be8d16dc371c4915fcbce2
duykasama <duykasama@gmail.com>
2024-02-16 14:47:53 +0700

    [Refactor][Duy] Modify exceptions

--- Domus.Common/Exceptions/MissingVnpaySettingsException.cs
+++ Domus.Common/Exceptions/MissingVnpaySettingsException.cs
@@ -2,7 +2,16 @@ namespace Domus.Common.Exceptions;
 
 public class MissingVnpaySettingsException : ArgumentNullException
 {
-	public MissingVnpaySettingsException() : base("Missing Vnpay settings")
-	{
-	}
+    private readonly string? _customMessage;
+    public override string Message => _customMessage ?? Message;
+
+    public MissingVnpaySettingsException(string customMessage)
+    {
+        _customMessage = customMessage;
+    }
+    
+    public MissingVnpaySettingsException()
+    {
+        _customMessage = "Missing vnpay settings";
+    }
 }
