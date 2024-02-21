06a6d438322bace6b5a29d9ae0a9202d291f891f
HaiHungNguyenn <hainhse173100@fpt.edu.vn>
2024-02-21 17:00:48 +0700

    [PackageManagement][Hai] fix typo
    degtail -> detail

--- Domus.Service/Models/Requests/OfferedPackages/PackageRequest.cs
+++ Domus.Service/Models/Requests/OfferedPackages/PackageRequest.cs
@@ -6,7 +6,8 @@ namespace Domus.Service.Models.Requests.OfferedPackages;
 public class PackageRequest
 {
     public List<Guid> ServiceIds { get; set; } 
-    public List<Guid> ProductDetgailIds { get; set; } 
+    
+    public List<Guid> ProductDetailIds { get; set; } 
     public string? Name { get; set; }
     public double? Discount { get; set; }
     public List<IFormFile>? Images { get; set; } = new List<IFormFile>();
