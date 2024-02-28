703ace964f2787870ae0be54cff92aef567383a2
duykasama <duykasama@gmail.com>
2024-02-29 02:18:50 +0700

    [Quotations mangement][Duy] Add package when creating quotations

--- Domus.Service/Models/Requests/Quotations/CreateQuotationRequest.cs
+++ Domus.Service/Models/Requests/Quotations/CreateQuotationRequest.cs
@@ -4,6 +4,8 @@ public class CreateQuotationRequest
 {
     public DateTime? ExpireAt { get; set; }
 
+    public Guid PackageId { get; set; }
+
     public ICollection<ProductDetailInCreatingQuotationRequest> ProductDetails { get; set; } = new List<ProductDetailInCreatingQuotationRequest>();
 
     public ICollection<Guid> Services { get; set; } = new List<Guid>();
