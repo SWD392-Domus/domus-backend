9af16dc48cf90a5762c3104c08c1aef8d5c29774
duykasama <duykasama@gmail.com>
2024-03-17 14:44:30 +0700

    [Hotfix][Duy] Return user information when logging in

--- Domus.Service/Implementations/AuthService.cs
+++ Domus.Service/Implementations/AuthService.cs
@@ -2,6 +2,7 @@ using System.Text.RegularExpressions;
 using AutoMapper;
 using Domus.Common.Helpers;
 using Domus.DAL.Interfaces;
+using Domus.Domain.Dtos;
 using Domus.Domain.Entities;
 using Domus.Service.Constants;
 using Domus.Service.Exceptions;
@@ -73,7 +74,8 @@ public class AuthService : IAuthService
 			}
 		};
 
-		return new ServiceActionResult(true) { Data = response };
+	    var returnedUser = _mapper.Map<DtoDomusUser>(user);
+		return new ServiceActionResult(true) { Data = new { userInfo = returnedUser, token = response } };
     }
 
     public async Task<ServiceActionResult> RefreshTokenAsync(RefreshTokenRequest request)
