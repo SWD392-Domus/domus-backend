061a395113f6a6ac4c64587c72348a13eb993c66
HaiHungNguyenn <hainhse173100@fpt.edu.vn>
2024-03-20 18:12:05 +0700

    hotfix/email-file-location

--- Domus.Service/Constants/EmailTemplatePathConstants.cs
+++ Domus.Service/Constants/EmailTemplatePathConstants.cs
@@ -9,10 +9,10 @@ public static class EmailTemplatePathConstants
     public static readonly string SignedContractEmailPath;
     
     private static readonly string BasePath = AppDomain.CurrentDomain.BaseDirectory ?? throw new ArgumentNullException("App Domain Not Found");
-    private const string RELATIVE_PATH_TO_OTP_EMAIL = @"EmailTemplates\OtpEmailTemplate.html";
-    private const string RELATIVE_PATH_TO_PASSWORD_EMAIL = @"EmailTemplates\PasswordEmailTemplate.html";
-    private const string RELATIVE_PATH_TO_CONTRACT_EMAIL = @"EmailTemplates\ContractEmailTemplate.html";
-    private const string RELATIVE_PATH_TO_SIGNED_CONTRACT_EMAIL = @"EmailTemplates\SignedContractEmailTemplate.html";
+    private const string RELATIVE_PATH_TO_OTP_EMAIL = @"EmailTemplates/OtpEmailTemplate.html";
+    private const string RELATIVE_PATH_TO_PASSWORD_EMAIL = @"EmailTemplates/PasswordEmailTemplate.html";
+    private const string RELATIVE_PATH_TO_CONTRACT_EMAIL = @"EmailTemplates/ContractEmailTemplate.html";
+    private const string RELATIVE_PATH_TO_SIGNED_CONTRACT_EMAIL = @"EmailTemplates/SignedContractEmailTemplate.html";
     static EmailTemplatePathConstants()
     {
         OtpEmailPath = Path.Combine(BasePath, RELATIVE_PATH_TO_OTP_EMAIL);
